d65e4aa1a283c7a4f7eafb906f035e6b760370e7
HeHHeyboi <thanathatwave@gmail.com>
2025-04-17 22:20:13 +0700

    Remove comment line

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,6 @@
 local M = {
 	'nvim-telescope/telescope.nvim',
 	tag = '0.1.8',
-	-- or                              , branch = '0.1.x',
-	--
-	--
 	dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' },
 	config = function()
 		local open_trouble = require("trouble.sources.telescope").open
