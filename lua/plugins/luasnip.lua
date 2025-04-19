f2bf331d7809f26d330fe16582aa650d3762e926
HeHHeyboi <thanathatwave@gmail.com>
2025-04-19 14:06:10 +0700

    Change to latest version

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,7 +1,13 @@
 return {
 	"L3MON4D3/LuaSnip",
 	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- version = function()
+	-- 	if vim.uv.os_uname == "Windows_NT" then
+	-- 		return ""
+	-- 	end
+	-- 	return "v2.*"
+	-- end
+	-- Replace <CurrentMajor> by the latest released major (first number of latest release)
 	-- install jsregexp (optional!).
 	-- build = "make install_jsregexp"
 	dependencies = { "rafamadriz/friendly-snippets" },
