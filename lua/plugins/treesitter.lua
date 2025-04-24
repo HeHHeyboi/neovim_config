069b63afc7abc6018c142b5fbfcfa5884de33fc2
HeHHeyboi <thanathatwave@gmail.com>
2025-04-25 01:11:53 +0700

    Fix treesitter highligh need to call "nvim-treesitter.configs"

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,39 +3,29 @@ local M =
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = { "gitattributes", "git_config", "git_rebase", "gitcommit", "gdscript", "c_sharp",
-				"cpp", "java", "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "go", "odin" },
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = { "gitattributes", "git_config", "git_rebase", "gitcommit", "gdscript", "c_sharp",
+					"cpp", "java", "c", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "go", "odin" },
 
-			sync_install = false,
-			modules = {},
-			ignore_install = {},
+				sync_install = false,
+				modules = {},
+				ignore_install = {},
 
-			auto_install = false,
+				auto_install = false,
 
-			indent = {
-				enable = true,
-			},
+				indent = {
+					enable = true,
+				},
 
-			highlight = {
-				enable = true,
+				highlight = {
+					enable = true,
 
-				additional_vim_regex_highlighting = false,
-			},
-		},
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end
 	},
-	-- {
-	-- 	'nvim-treesitter/nvim-treesitter-refactor',
-	-- 	dependencies = { 'nvim-treesitter/nvim-treesitter' },
-	-- 	config = function()
-	-- 		---@diagnostic disable-next-line: missing-fields
-	-- 		require 'nvim-treesitter.configs'.setup {
-	-- 			refactor = {
-	-- 				highlight_definitions = { enable = true },
-	-- 				highlight_current_scope = { enable = false },
-	-- 			}
-	-- 		}
-	-- 	end
-	-- }
 }
 return M
