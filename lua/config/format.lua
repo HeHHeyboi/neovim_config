0fb7340ed09b1e8f401910d69803fe1b3f61c3bf
HeHHeyboi <thanathatwave@gmail.com>
2025-05-05 13:20:30 +0700

    Use job:wait not to run async for formating

--- lua/config/format.lua
+++ lua/config/format.lua
@@ -12,20 +12,70 @@ Custom_format = {
 	},
 }
 
+-- function Format_file(formatter, args)
+-- 	local name = vim.fn.bufname("%")
+-- 	local exe = vim.fn.exepath(formatter)
+-- 	if exe == "" then
+-- 		notify("Formatter not found: " .. formatter, vim.log.levels.ERROR)
+-- 		return
+-- 	end
+--
+-- 	local cmd = { exe, name }
+-- 	if args == "" then
+-- 		cmd = { exe, args, name }
+-- 	end
+--
+-- 	notify("Start Fomat", vim.log.levels.INFO)
+-- 	local output = vim.system(cmd)
+-- 	local code = vim.v.shell_error
+--
+-- 	if code ~= 0 then
+-- 		notify(output, vim.log.levels.ERROR)
+-- 	else
+-- 		-- Reload the file content after formatting
+-- 		vim.cmd("edit")
+-- 		notify("Format " .. name .. " success", vim.log.levels.INFO)
+-- 	end
+-- end
+
 function Format_file(formatter, args)
 	local name = vim.fn.bufname("%")
-	local cmd = { vim.fn.exepath(formatter), args, name }
-	if args == "" then
-		cmd = { vim.fn.exepath(formatter), name }
+	local exe = vim.fn.exepath(formatter)
+	if exe == "" then
+		notify("Formatter not found: " .. formatter, vim.log.levels.ERROR)
+		return
 	end
-	vim.system(cmd, {}, function(out)
-		if out.code ~= 0 then
-			notify(out.stderr, vim.log.levels.ERROR)
-			return
-		end
-		vim.defer_fn(function()
-			vim.cmd("edit")
-			notify("Format Success", vim.log.levels.INFO)
-		end, 0)
-	end)
+
+	local cmd = { exe }
+	if args ~= "" then
+		table.insert(cmd, args)
+	end
+	table.insert(cmd, name)
+	local job = vim.system(cmd, {})
+	local result = job:wait()
+	if result.code ~= 0 then
+		notify(result.stderr, vim.log.levels.ERROR)
+		return
+	end
+	vim.cmd("edit")
+	notify("format " .. name .. " success", vim.log.levels.INFO)
 end
+
+-- local job = vim.fn.jobstart(cmd, {
+-- 	on_stdout = function()
+-- 		vim.cmd("edit")
+-- 		notify("Format " .. name .. " Success", vim.log.levels.INFO)
+-- 	end
+-- })
+-- vim.fn.jobwait({ job })
+
+-- vim.system(cmd, {}, function(out)
+-- 	if out.code ~= 0 then
+-- 		notify(out.stderr, vim.log.levels.error)
+-- 		return
+-- 	end
+-- 	vim.defer_fn(function()
+-- 		vim.cmd("edit")
+-- 		notify("format " .. name .. " success", vim.log.levels.info)
+-- 	end, 0)
+-- end)
