c25218efb4162eeafa79a76f5936fc91aa4afa89
HeHHeyboi <thanathatwave@gmail.com>
2025-05-22 21:20:01 +0700

    Normalize path in Windows

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,6 +8,22 @@ return {
 		-- harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
 		-- REQUIRED
 		harpoon:setup({
+			default = {
+				display = function(list_item)
+					local norm = vim.fs.normalize(list_item.value)
+					local cwd = vim.uv.cwd()
+					if cwd == nil then
+						return norm
+					end
+
+					local relative = vim.fs.relpath(cwd, norm)
+					if relative == nil then
+						return list_item.value
+					end
+
+					return relative
+				end
+			},
 			settings = {
 				save_on_toggle = false,
 				sync_on_ui_close = true,
@@ -25,6 +41,9 @@ return {
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 		vim.keymap.set("n", "<C-e>",
 			function() harpoon.ui:toggle_quick_menu(harpoon:list(), { border = "rounded", height_in_lines = 5 }) end)
+
+		-- vim.keymap.del("n", "<C-;>")
+		-- vim.keymap.del("n", "<C-'>")
 
 		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end, { desc = "harpoon select 1" })
 		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end, { desc = "harpoon select 2" })
