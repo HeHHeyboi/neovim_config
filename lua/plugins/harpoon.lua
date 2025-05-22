1b65f8ba60614c76ed9e2730190467a0d188a611
HeHHeyboi <thanathatwave@gmail.com>
2025-05-22 16:20:47 +0700

    Add new keymap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,20 +10,27 @@ return {
 		harpoon:setup({
 			settings = {
 				save_on_toggle = false,
-				sync_on_ui_close = true
+				sync_on_ui_close = true,
+				key = function()
+					local cwd = vim.uv.cwd()
+					if cwd == nil then
+						return ""
+					end
+					return cwd
+				end
 			},
-			key = function()
-				return vim.uv.cwd()
-			end
 		})
 		-- REQUIRED
 
 		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<C-e>",
+			function() harpoon.ui:toggle_quick_menu(harpoon:list(), { border = "rounded", height_in_lines = 5 }) end)
 
-		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end, { desc = "harpoon select 1" })
+		vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end, { desc = "harpoon select 2" })
+		vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end, { desc = "harpoon select 3" })
+		vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end, { desc = "harpoon select 4" })
+		vim.keymap.set("n", "<C-'>", function() harpoon:list():select(5) end, { desc = "harpoon select 5" })
 		-- vim.keymap.set("n", "<leader>u", function() harpoon:list():select(1) end)
 		-- vim.keymap.set("n", "<leader>i", function() harpoon:list():select(2) end)
 		-- vim.keymap.set("n", "<leader>o", function() harpoon:list():select(3) end)
