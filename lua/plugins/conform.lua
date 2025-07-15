7f0bb63fc24c366d1b80a72c24103b14f447e9c5
HeHHeyboi <thanathatwave@gmail.com>
2025-07-16 03:20:47 +0700

    Config conform to call mkview before & loadview after save

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,13 +13,19 @@ return {
 			default_format_opts = {
 				lsp_format = "fallback",
 			},
-			format_on_save = {
-				lsp_format = "fallback",
-				timeout_ms = 500,
-			},
-			format_after_save = {
-				lsp_format = "fallback",
-			},
+			format_on_save = function(bufnr)
+				vim.cmd("silent! mkview!")
+				return {
+					lsp_format = "fallback",
+					timeout_ms = 500,
+				}
+			end,
+			format_after_save = function(bufnr)
+				vim.cmd("silent! loadview")
+				return {
+					lsp_format = "fallback",
+				}
+			end,
 			formatters = {
 				csharpier = {
 					inherit = false,
