c729cfaee29ae41d48fcb1307e20b53d557c05f8
HeHHeyboi <thanathatwave@gmail.com>
2025-08-17 17:26:55 +0700

    Handle different for windows and linux

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,5 +1,20 @@
+local function jdtls_exe()
+	local path = "jdtls"
+	if vim.uv.os_uname().sysname == "Windows_NT" then
+		path = "C:/Users/thana/AppData/Local/nvim-data/mason/bin/jdtls.cmd"
+	end
+	return { path }
+end
+
+local function root_dir()
+	local file = vim.fs.find({ 'pom.xml', 'gradlew', '.git', 'mvnw', '.classpath' }, { upward = true })
+	local root = vim.fs.dirname(file[1])
+	print(root)
+	return root
+end
+
 local config = {
-	cmd = { 'C:/Users/thana/AppData/Local/nvim-data/mason/bin/jdtls.cmd' },
-	root_dir = vim.fs.dirname(vim.fs.find({ 'pom.xml', 'gradlew', '.git', 'mvnw', '.classpath' }, { upward = true })[1]),
+	cmd = jdtls_exe(),
+	root_dir = root_dir()
 }
 require('jdtls').start_or_attach(config)
