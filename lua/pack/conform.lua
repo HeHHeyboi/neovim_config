41f90381ca70a92577dcf4585322b76f9fd49812
HeHHeyboi <thanathatwave@gmail.com>
2026-09-14 13:30:58 +0700

    update gdscript formatter

--- lua/pack/conform.lua
+++ lua/pack/conform.lua
@@ -3,7 +3,7 @@ vim.pack.add({
 })
 require("conform").setup({
 	formatters_by_ft = {
-		gdscript = { "gdformat" },
+		gdscript = { "gdscript-formatter" },
 		cs = { "csharpier" },
 		hurl = { "hurlfmt" },
 	},
